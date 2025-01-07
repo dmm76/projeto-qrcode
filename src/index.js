80b662d5c67d8f97b06560a2e3cd0d754a267b86
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-01-07 16:21:07 -0300

    err

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import createQRCode from "./services/qr-code/create.js";
 
 import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
 import createPassword from "./services/password/create.js";
+import chalk from "chalk";
 
 async function main() {
-  prompt.get(promptSchemaMain, async (err, choose) =>{
-    if(choose.select == 1) await createQRCode();
-    if(choose.select == 2) await createPassword();
+  prompt.get(promptSchemaMain, async (err, choose) => {
+    if (err) console.log(chalk.red(err));
+
+    if (choose.select == 1) await createQRCode();
+    if (choose.select == 2) await createPassword();
   });
   prompt.start();
 }
